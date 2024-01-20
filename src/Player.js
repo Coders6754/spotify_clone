45d2ca4d6494e2f070feb17489223db957db3fd5
TechSiren <asinha27072002@gmailo.com>
2024-01-20 20:23:29 +0530

    api context

--- src/Player.js
+++ src/Player.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Player = () => {
   return (
     <div>
-      
+      <h1>Welcome to spotify</h1>
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,25 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+
+  //Action --> type, [payload] 
+  switch (action.type) {
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user
+      }
+    default:
+      return state;
+
+  }
+}
+
+export default reducer;
