dee1e479d0353416681ad61b426d6bf8bace752c
TechSiren <asinha27072002@gmailo.com>
2024-01-20 20:24:05 +0530

    working on api context

--- src/DataLayer.js
+++ src/DataLayer.js
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useReducer } from 'react';
 export const DataLayerContext = createContext();
 
 export const DataLayer = ({ initialState, reducer, children }) => (
-  <DataLayerContext.Provider value={useReducer(reducer, initialState)}>
+  <DataLayerContext.Provider value={useReducer (reducer, initialState)}>
     {children}
   </DataLayerContext.Provider>
 );
