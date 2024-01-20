ee4d28a852ebe2b370437ecb0022ea961bd142dd
TechSiren <asinha27072002@gmailo.com>
2024-01-20 18:57:13 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+  //Runs a code based a piece of some condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const token = hash.access_token;
+
+    console.log("I have a token", token);
+  }, []);
+
   return (
     <div className="app">
-     <Login/>
+      <Login />
     </div>
   );
 }
--- src/spotify.js
+++ src/spotify.js
@@ -11,3 +11,22 @@ const scopes = [
   "user-top-read",
   "user-modify-playback-state",
 ];
+
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((initial, item) => {
+      // #accessToken= mysupersecretKey&name=Anjali
+      let parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+
+    }, {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
