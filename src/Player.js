b54481d069fef0a5703e323581da661168bc4f5e
TechSiren <asinha27072002@gmailo.com>
2024-01-21 23:02:41 +0530

    outlining

--- src/Player.js
+++ src/Player.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Player.css";
+import Body from './Body';
+import Sidebar from './Sidebar';
 
 const Player = () => {
   return (
     <div className="player">
       <div className="player_body">
-        {/* Sidebar */}
-        {/* body */}
+        <Sidebar />
+        <Body />
       </div>
 
       {/* Footer */}
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <h1>Hello I am the sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
