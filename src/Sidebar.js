db78111d8bd5e31007c225f8f8137baf4cda2514
TechSiren <asinha27072002@gmailo.com>
2024-01-22 23:42:55 +0530

    working on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,7 @@ import "./Sidebar.css";
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <h1>Hello I am the sidebar</h1>
+      <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
     </div>
   );
 }
