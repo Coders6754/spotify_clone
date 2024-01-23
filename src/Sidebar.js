6c975af87d6d7a6f40be2fb58125d854d07ccc6e
TechSiren <asinha27072002@gmailo.com>
2024-01-23 21:02:07 +0530

    side

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,13 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from "@mui/icons-material/Search";
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import { useDataLayerValue } from "./DataLayer";
 
 
-const Sidebar = () => {
+function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+  console.log(playlists);
+
   return (
     <div className='sidebar'>
       <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
@@ -17,7 +21,7 @@ const Sidebar = () => {
       <strong className="sidebar__title">PLAYLISTS</strong>
       <hr />
 
-      
+
     </div>
   );
 }
